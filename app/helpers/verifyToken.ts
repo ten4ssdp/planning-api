ca0a71b7085c3fa481255990499756f5a4a70687
maximeoger <maximeoger93@gmail.com>
2020-01-31 20:58:09 +0100

    wip

---
+++ app/helpers/verifyToken.ts
@@ -0,0 +1,14 @@
+function verify(req, res, next): void {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    // Forbiden
+    res.sendStatus(403);
+  }
+}
+
+module.exports = verify;
