717f4f8c91236e0831f6af944e78c445334a7f7a
maximeoger <maximeoger93@gmail.com>
2020-02-11 07:11:42 +0100

    add WORKDIR to node dockerfile

--- app/connexion.ts
+++ app/connexion.ts
@@ -1,6 +1,5 @@
 (function(){
     var Sequelize = require('sequelize');
-    
     module.exports = new Sequelize(`postgres://${process.env.DB_ROOT_USERNAME}:${process.env.DB_ROOT_PASSWORD}@db:5432/ssdp`);
 })()
 
