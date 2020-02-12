b0595c392356899b27173a7b2d1fc93532b954bc
maximeoger <maximeoger93@gmail.com>
2020-02-12 07:57:07 +0100

    gestion de la generation/validation du token + routes

--- app/index.ts
+++ app/index.ts
@@ -1,28 +1,22 @@
 (function() {
+  
   const express = require('express');
   const app = express();
   const db = require('./connexion');
-//  const userRoutes = require('./routes/user');
+
+  const indexRoutes = require('./routes/index');
+  const userRoutes = require('./routes/user');
 
   db.authenticate()
     .then(() => console.log('Database connected ...'))
     .catch(err => console.log('Unable to connect to the database', err));
 
-  app.get('/api', (req, res) => {
-    res.json({
-      message: "Bienvenue sur l'api SSDP",
-      description:
-        "Elle contient l'ensemble des données utiles pour l'application du samu social de paris",
-    });
-  });
-
-  app.get('/api/user/', (req, res) => {
-    res.json([{ name: 'Toto' }]);
-  });
-
-  //app.use('/api/user', userRoutes);
+  // routes
+  app.use('/api', indexRoutes);
+  app.use('/api/user', userRoutes);
 
   const PORT = process.env.PORT || '5000';
 
   app.listen(PORT, console.log('server started at port ' + PORT));
+
 })();
