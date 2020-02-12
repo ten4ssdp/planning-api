881480dc2f81b66fbe43befccc84a168edc3db35
maximeoger <maximeoger93@gmail.com>
2020-02-12 07:57:07 +0100

    gestion de la generation/validation du token + routes

--- app/connexion.ts
+++ app/connexion.ts
@@ -3,13 +3,3 @@
     module.exports = new Sequelize(`postgres://${process.env.DB_ROOT_USERNAME}:${process.env.DB_ROOT_PASSWORD}@db:5432/ssdp`);
 })()
 
-
-
-
-// problème de connexion à la bdd 
-//  peut etre c'est TypeScript qui compile mal ce qui entraine des bugs dans les modules depuis index.ts
-//  peut etre c'est les variables d'environnement
-//  peut etre c'est autre chose
-//  en tout cas le console log ne s'affiche pas ICI, 
-//  et la connexion plante une fois sur 2 quand on fait docker-compose
-//  le mystère reste entier
--- app/routes/user.ts
+++ app/routes/user.ts
@@ -2,54 +2,29 @@
   const express = require('express');
   //const User = require("../models/User");
   const jwt = require('jsonwebtoken');
-  const verifyToken = require('./helpers/verifyToken');
+  const verifyToken = require('../helpers/verifyToken');
 
-  const router = express.router();
+  const router = express.Router();
 
-  router.post('/register', async function(req, res) {
-    try {
-    } catch (err) {}
-  });
+  // vérifier le token pour chaque route
+  router.use(verifyToken);
 
-  router.post('/login', async function(req, res) {
-    try {
-      jwt.sign(
-        {
-          email: req.params.email,
-          password: req.params.password,
-        },
-        'secretKey',
-        (err, token) => {
-          if (err) {
-            res.sendStatus(401);
-          } else {
-            res.json({ token });
-          }
-        },
-      );
-    } catch (err) {}
-  });
+  // Récupérer la liste des users (par défaut)
+  router.get('/', async function(req, res) {
+    try{
+        jwt.verify(req.token, 'secretKey', (err, authData) => {
+            if (err) {
+              res.sendStatus(403);
+            } else {
+              res.json({
+                message: 'Liste des Users',
+                authData,
+              });
+            }
+          });
+    }catch(err){
 
-  router.get('/', verifyToken, async function(req, res) {
-    res.json({
-      message: 'GET sur les user',
-    });
-    /*
-          try{
-              jwt.verify(req.token, 'secretKey', (err, authData) => {
-                  if (err) {
-                    res.sendStatus(403);
-                  } else {
-                    res.json({
-                      message: 'planning de la semaine',
-                      authData,
-                    });
-                  }
-                });
-          }catch(err){
-  
-          }
-          */
+    }    
   });
 
   module.exports = router;
