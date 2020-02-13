b85dc986a41fafb401a379cf037ebeef2617ec4a
maximeoger <maximeoger93@gmail.com>
2020-02-13 08:00:15 +0100

    add Role table + create association

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,36 +1,56 @@
-const { Model, DataTypes } = require('sequelize');
+(function(){
+    const { DataTypes, Model } = require('sequelize');
+    const sequelize = require('../connexion');
 
+    const Role = require('./Role');
 
-class User extends Model {
-    public user_id!: number;
-    public user_name!: string;
-    public user_lastname!: string;
-    public user_email!: string;
-    public user_password!: string;
-}
+    class User extends Model {
+        public id!: number;
+        public user_name!: string;
+        public user_lastname!: string;
+        public user_email!: string;
+        public user_role!: string;
+        public user_password!: string;
+    }
+    
+    User.init({
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        user_name: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+            notEmpty: true,
+        },
+        user_lastname: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+            notEmpty: true,
+        },
+        user_email: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+            isEmail: true,
+        },
+    }, {
+        underscored: true,
+        modelName: 'user',
+        sequelize
+    })
 
-User.init({
-    user_id: {
-        type: new DataTypes.INTEGER.UNSIGNED,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    user_name: {
-        type: new DataTypes.STRING(128),
-        allowNull: false,
-    },
-    user_lastname: {
-        type: new DataTypes.STRING(128),
-        allowNull: false,
-    },
-    user_email: {
-        type: new DataTypes.STRING(128),
-        allowNull: false
-    },
+    User.belongsTo(Role);
 
-}, {
-    tableName: 'user',
-    sequelize: require('../connexion')
-})
-
-module.exports = User;
+    sequelize.sync()
+    .then(() => User.create({
+        user_name: "Maxime",
+        user_lastname: "Oger",
+        user_email: "maxime.oger@hetic.net",
+        user_password: "1234"
+    }))
+    .then(user => console.log(user.toJSON()))
+    .catch(err => {throw err})
+    
+    module.exports = User;
+})()
