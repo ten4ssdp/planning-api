16775ed183c97451b4327313ccee9ec9a7209db8
Kevin <kevin.manssat@hotmail.fr>
2020-02-15 13:56:03 +0100

    change require to import

--- app/models/Role.ts
+++ app/models/Role.ts
@@ -1,34 +1,34 @@
-(function(){
-    const { DataTypes, Model } = require('sequelize');
+import { DataTypes, Model } from 'sequelize';
+import db from '../connexion';
 
-    const sequelize = require('../connexion');
+class Role extends Model {
+  public id!: number;
+  public name!: string;
+  public level!: string;
+}
 
-    class Role extends Model {
-        public id! : number;
-        public name! : string;
-        public level! : string; 
-    }
+Role.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+      notEmpty: true,
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    modelName: 'role',
+    db,
+  },
+);
 
-    Role.init({
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING(128),
-            allowNull: false,
-            notEmpty: true,
-        },
-        level: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    }, {
-        underscored: true,
-        modelName: 'role',
-        sequelize
-    })
-
-    module.exports = Role
-})()
+export default Role;
--- app/routes/user.ts
+++ app/routes/user.ts
@@ -1,31 +1,26 @@
-(function() {
-  const express = require('express');
-  const User = require("../models/User");
-  const jwt = require('jsonwebtoken');
-  const verifyToken = require('../helpers/verifyToken');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import verifyToken from '../helpers/verifyToken';
 
-  const router = express.Router();
+const router = express.Router();
 
-  // vérifier le token pour chaque route
-  router.use(verifyToken);
+// vérifier le token pour chaque route
+router.use(verifyToken);
 
-  // Récupérer la liste des users (par défaut)
-  router.get('/', async function(req, res) {
-    try{
-        jwt.verify(req.token, 'secretKey', (err, authData) => {
-            if (err) {
-              res.sendStatus(403);
-            } else {
-              res.json({
-                message: 'Liste des Users',
-                authData,
-              });
-            }
-          });
-    }catch(err){
+// Récupérer la liste des users (par défaut)
+router.get('/', async function(req, res) {
+  try {
+    jwt.verify(req.token, 'secretKey', (err, authData) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        res.json({
+          message: 'Liste des Users',
+          authData,
+        });
+      }
+    });
+  } catch (err) {}
+});
 
-    }    
-  });
-
-  module.exports = router;
-})();
+export default router;
