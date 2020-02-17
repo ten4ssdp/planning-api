9e54d5ea44eb5546a4081d9cd849a5af8706cc0e
Kevin <kevin.manssat@hotmail.fr>
2020-02-17 23:07:39 +0100

    add dokerfile + update env var

--- app/connexion.ts
+++ app/connexion.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export default new Sequelize(
-  `postgres://${process.env.DB_ROOT_USERNAME}:${process.env.DB_ROOT_PASSWORD}@db:5432/ssdp`,
+  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/ssdp`,
   {
     sync: { force: true },
   },
