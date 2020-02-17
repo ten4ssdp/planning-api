a41fcc5a0363ccb5f5f1aa29e8c64dc6db5cf4ab
maximeoger <maximeoger93@gmail.com>
2020-02-17 11:05:45 +0100

    fix conflict rebase

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,36 +1,59 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
+(function() {
+  const express = require('express');
+  const jwt = require('jsonwebtoken');
+  const router = express.Router({ mergeParams: true });
+  const user = require('../models/User');
 
-const router = express.Router();
+  async function verifyCredentials(req, res, next): void {
+    console.log(req.params);
+    // let found = user.findOne({
+    //   where: {
+    //     email: req.params.email,
+    //     password: req.params.password,
+    //   }
+    // });
+    res.send('oui');
+    return;
+    if (found) {
+      req.user = found;
+      next();
+    } else {
+      // mauvais credentials
+      req.status(401).send('Email ou Mot de Passe incorrect.');
+    }
+  }
 
-// Générer un token
-router.post('/login', async function(req, res) {
-  try {
-    jwt.sign(
-      {
-        email: req.params.email,
-        password: req.params.password,
-      },
-      'secretKey',
-      { expiresIn: '24h' },
-      (err, token) => {
-        if (err) {
-          res.sendStatus(401);
-        } else {
-          res.json({ token });
-        }
-      },
-    );
-  } catch (err) {}
-});
-
-// Index de l'api
-router.get('/', async function(req, res) {
-  res.json({
-    message: "Bienvenue sur l'api SSDP",
-    description:
-      "Elle contient l'ensemble des données utiles pour l'application du samu social de paris",
+  // Générer un token
+  router.post('/login', verifyCredentials, async function(req, res) {
+    //console.log(req.user)
+    return;
+    try {
+      jwt.sign(
+        {
+          email: req.params.email,
+          password: req.params.password,
+        },
+        'secretKey',
+        { expiresIn: '24h' },
+        (err, token) => {
+          if (err) {
+            res.sendStatus(401);
+          } else {
+            res.json({ token });
+          }
+        },
+      );
+    } catch (err) {}
   });
-});
 
-export default router;
+  // Index de l'api
+  router.get('/', async function(req, res) {
+    res.json({
+      message: "Bienvenue sur l'api SSDP",
+      description:
+        "Elle contient l'ensemble des données utiles pour l'application du samu social de paris",
+    });
+  });
+
+  module.exports = router;
+})();
