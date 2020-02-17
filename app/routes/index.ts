368e0651fb8e613e2ff011ba15868dcf5e264fa9
maximeoger <maximeoger93@gmail.com>
2020-02-17 11:22:09 +0100

    add import synthax in user routes

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,59 +1,58 @@
-(function() {
-  const express = require('express');
-  const jwt = require('jsonwebtoken');
-  const router = express.Router({ mergeParams: true });
-  const user = require('../models/User');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import user from '../models/User';
 
-  async function verifyCredentials(req, res, next): void {
-    console.log(req.params);
-    // let found = user.findOne({
-    //   where: {
-    //     email: req.params.email,
-    //     password: req.params.password,
-    //   }
-    // });
-    res.send('oui');
-    return;
-    if (found) {
-      req.user = found;
-      next();
-    } else {
-      // mauvais credentials
-      req.status(401).send('Email ou Mot de Passe incorrect.');
-    }
+const router = express.Router({ mergeParams: true });
+
+async function verifyCredentials(req, res, next): void {
+  console.log(req.params);
+  // let found = user.findOne({
+  //   where: {
+  //     email: req.params.email,
+  //     password: req.params.password,
+  //   }
+  // });
+  res.send('oui');
+  return;
+  if (found) {
+    req.user = found;
+    next();
+  } else {
+    // mauvais credentials
+    req.status(401).send('Email ou Mot de Passe incorrect.');
   }
+}
 
-  // Générer un token
-  router.post('/login', verifyCredentials, async function(req, res) {
-    //console.log(req.user)
-    return;
-    try {
-      jwt.sign(
-        {
-          email: req.params.email,
-          password: req.params.password,
-        },
-        'secretKey',
-        { expiresIn: '24h' },
-        (err, token) => {
-          if (err) {
-            res.sendStatus(401);
-          } else {
-            res.json({ token });
-          }
-        },
-      );
-    } catch (err) {}
+// Générer un token
+router.post('/login', verifyCredentials, async function(req, res) {
+  //console.log(req.user)
+  return;
+  try {
+    jwt.sign(
+      {
+        email: req.params.email,
+        password: req.params.password,
+      },
+      'secretKey',
+      { expiresIn: '24h' },
+      (err, token) => {
+        if (err) {
+          res.sendStatus(401);
+        } else {
+          res.json({ token });
+        }
+      },
+    );
+  } catch (err) {}
+});
+
+// Index de l'api
+router.get('/', async function(req, res) {
+  res.json({
+    message: "Bienvenue sur l'api SSDP",
+    description:
+      "Elle contient l'ensemble des données utiles pour l'application du samu social de paris",
   });
+});
 
-  // Index de l'api
-  router.get('/', async function(req, res) {
-    res.json({
-      message: "Bienvenue sur l'api SSDP",
-      description:
-        "Elle contient l'ensemble des données utiles pour l'application du samu social de paris",
-    });
-  });
-
-  module.exports = router;
-})();
+export default router;
