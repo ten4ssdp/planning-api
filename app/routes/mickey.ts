1c0338aca1c9ead3a6b86e3772c85869f005535b
ptp <paartheepan94@gmail.com>
2020-02-18 17:26:13 +0100

    feat(wip): dispatch visits

--- app/routes/mickey.ts
+++ app/routes/mickey.ts
@@ -3,81 +3,83 @@ import User from '../models/User';
 import Role from '../models/Role';
 import Sector from '../models/Sector';
 import { Sequelize } from 'sequelize';
+import Mickey, {
+  getHotelsAndVisits,
+  dispatchHotelsToTeams,
+  getTeamsGroupedBySector,
+} from '../Mickey';
+import Team from '../models/Team';
 import Visit from '../models/Visit';
 import Hotel from '../models/Hotel';
-import Mickey from '../Mickey';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  let users: User[];
-  (async (): Promise<User[]> => {
-    users = await User.findAll({
-      include: [
-        {
-          model: Role,
-          where: {
-            name: 'Intervenant Terrain',
+  const users: User[] = await User.findAll({
+    include: [
+      {
+        model: Role,
+        where: {
+          name: 'Intervenant Terrain',
+        },
+      },
+    ],
+  });
+  return res.send(users);
+});
+router.get('/sector', async (req, res) => {
+  const sectors = await Sector.findAll({
+    group: ['sector.id'],
+  })
+    .map(sector => sector.name)
+    .reduce(async (sectorsPromise, sector) => {
+      const sectorsMutated = await sectorsPromise;
+
+      const sectorsRows = await User.findAll({
+        order: [Sequelize.fn('RANDOM')],
+        include: [
+          {
+            model: Role,
+            where: {
+              name: 'Intervenant Terrain',
+            },
           },
-        },
-      ],
-    });
-    res.send(users);
-    return users;
-  })();
-});
-router.get('/sector', (req, res) => {
-  (async (): Promise<Sector[]> => {
-    const sectors = await Sector.findAll({
-      group: ['sector.id'],
-    })
-      .map(sector => sector.name)
-      .reduce(async (sectorsPromise, sector) => {
-        const sectorsMutated = await sectorsPromise;
-
-        const sectorsRows = await User.findAll({
-          order: [Sequelize.fn('RANDOM')],
-          include: [
-            {
-              model: Role,
-              where: {
-                name: 'Intervenant Terrain',
-              },
+          {
+            model: Sector,
+            where: {
+              name: sector,
             },
-            {
-              model: Sector,
-              where: {
-                name: sector,
-              },
-            },
-          ],
-        });
+          },
+        ],
+      });
 
-        sectorsMutated[sector] = [
-          ...(sectorsMutated[sector] || []),
-          ...sectorsRows,
-        ];
+      sectorsMutated[sector] = [
+        ...(sectorsMutated[sector] || []),
+        ...sectorsRows,
+      ];
 
-        return sectorsMutated;
-      }, {});
-    res.send(sectors);
-    return sectors;
-  })();
+      return sectorsMutated;
+    }, {});
+  return res.send(sectors);
 });
 
-router.get('/hotels', (req, res) => {
-  (async (): Promise<Visit[]> => {
-    const hotels = await Hotel.findAll({
-      include: [
-        {
-          model: Visit,
-        },
-      ],
-      order: [[Visit, 'date', 'ASC NULLS FIRST']],
-    });
-    res.send(hotels);
-    return hotels;
-  })();
+router.get('/hotels', async (req, res) => {
+  const hotels = await getHotelsAndVisits();
+  res.send(hotels);
+});
+router.get('/teams', async (req, res) => {
+  const teams = await getTeamsGroupedBySector();
+  res.send(teams);
+});
+router.get('/visits', async (req, res) => {
+  Mickey.init();
+  const hotels = await getHotelsAndVisits();
+  const teams = await getTeamsGroupedBySector();
+  const firstSectorHotels = hotels[Object.keys(hotels)[0]];
+  const firstSectorTeams = teams[Object.keys(teams)[0]];
+  console.log(teams[Object.keys(teams)[0]]);
+  const visits = dispatchHotelsToTeams(firstSectorTeams, firstSectorHotels);
+  res.send(visits);
 });
 router.get('/main', (req, res) => {
   Mickey.init();
