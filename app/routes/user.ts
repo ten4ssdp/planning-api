6949bfc8f5846c95bbf68f91f6e5a6e649fe2073
maximeoger <maximeoger93@gmail.com>
2020-02-20 13:20:30 +0100

    handle crud for user

--- app/routes/user.ts
+++ app/routes/user.ts
@@ -29,18 +29,79 @@ router.delete('/:id', async (req, res) => {
 // modifier un user
 router.put('/:id(\\d+)', async (req, res) => {
   try {
-    const userFound = await User.findOne({
-      where: { id: req.params.id },
-      raw: true,
-    });
-
     const hash = await bcrypt.hash(req.body.password, 10);
 
-    if (!userFound) {
-      res.status(501).json({ error: "Cet utilisateur n'existe pas." });
+    const result = await User.update(
+      {
+        name: req.body.name,
+        lastname: req.body.lastname,
+        address: req.body.address,
+        email: req.body.email,
+        password: hash,
+        roleId: req.body.roleId,
+        sectorId: req.body.sectorId,
+      },
+      { where: { id: req.params.id }, returning: true },
+    );
+
+    const [rowsUpdate, [updatedUser]] = result;
+
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
     } else {
-      const updatedUser = await User.update(
-        {
+      res.status(400).json({ error: "Cet utilisateur n'existe pas." });
+    }
+  } catch (err) {
+    res.status(500);
+  }
+});
+
+router.get(
+  '/:id(\\d+)',
+  async (req, res): Promise<void> => {
+    try {
+      const userFound: User = await User.findOne({
+        where: { id: req.params.id },
+        include: [
+          { model: Sector, attributes: ['id', 'name'] },
+          { model: Role, attributes: ['id', 'name'] },
+        ],
+        attributes: ['id', 'name', 'lastname', 'address'],
+        raw: true,
+        nest: true,
+      });
+
+      if (!userFound) {
+        res.status(501).json({ error: "Cet utilisateur n'existe pas." });
+      }
+
+      res.status(200).json(userFound);
+    } catch (err) {
+      res.status(500);
+    }
+  },
+);
+
+// créer un user
+router.post(
+  '/create',
+  async (req, res): Promise<void> => {
+    try {
+      const hash = await bcrypt.hash(req.body.password, 10);
+
+      const result = await User.findOrCreate({
+        where: { email: req.body.email },
+        raw: true,
+        attributes: [
+          'id',
+          'name',
+          'lastname',
+          'address',
+          'email',
+          'roleId',
+          'sectorId',
+        ],
+        defaults: {
           name: req.body.name,
           lastname: req.body.lastname,
           address: req.body.address,
@@ -49,70 +110,29 @@ router.put('/:id(\\d+)', async (req, res) => {
           roleId: req.body.roleId,
           sectorId: req.body.sectorId,
         },
-        { where: { id: req.params.id }, returning: true },
-      );
-
-      res.status(200);
-    }
-  } catch (err) {
-    res.status(500);
-  }
-});
-
-router.get('/:id(\\d+)', async (req, res) => {
-  try {
-    const userFound: User = await User.findByPk(req.params.id, { raw: true });
-
-    if (!userFound) {
-      res.status(501).json({ error: "Cet utilisateur n'existe pas." });
-    }
-
-    res.status(200).json({
-      name: userFound.name,
-      lastname: userFound.lastname,
-      role: {
-        id: userFound['role.id'],
-        name: userFound['role.name'],
-        level: userFound['role.level'],
-      },
-      sector: {
-        id: userFound['sector.id'],
-        name: userFound['sector.name'],
-      },
-    });
-  } catch (err) {
-    res.status(500);
-  }
-});
-
-// créer un user
-router.post('/create', async (req, res) => {
-  try {
-    const hash = await bcrypt.hash(req.body.password, 10);
-
-    const userExists = await User.findOne({
-      where: { email: req.body.email },
-    });
-
-    if (userExists) {
-      res.status(501).send('Utilisateur déjà existant.');
-    } else {
-      const createdUser = await User.create({
-        name: req.body.name,
-        lastname: req.body.lastname,
-        address: req.body.address,
-        email: req.body.email,
-        password: hash,
-        roleId: req.body.roleId,
-        sectorId: req.body.sectorId,
       });
 
-      res.status(201).send(createdUser);
+      const [model, created] = result;
+
+      if (created === false) {
+        res.status(500).json({ error: 'Utilisateur déjà existant.' });
+      } else {
+        res.status(201).json({
+          created: {
+            id: model.dataValues.id,
+            name: model.dataValues.name,
+            lastname: model.dataValues.lastname,
+            email: model.dataValues.lastname,
+            roleId: model.dataValues.roleId,
+            sectorId: model.dataValues.sectorId,
+          },
+        });
+      }
+    } catch (err) {
+      res.status(500);
     }
-  } catch (err) {
-    res.status(500);
-  }
-});
+  },
+);
 
 // Récupérer la liste des users (par défaut)
 router.get('/', async function(req, res): Promise<void> {
