b85f29d8fbb315faca2353f6226f897b92491c9a
maximeoger <maximeoger93@gmail.com>
2020-06-30 16:00:13 +0200

    change DB name when testing

--- app/connexion.ts
+++ app/connexion.ts
@@ -1,7 +1,10 @@
 import { Sequelize } from 'sequelize';
 
+// pour avoir une base de données différente pour l'environement de test
+let db_name = process.env.NODE_ENV === "test" ? "ssdp-test" : "ssdp";
+
 export default new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/ssdp`,
+  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/${db_name}`,
   {
     sync: { force: true },
   },
