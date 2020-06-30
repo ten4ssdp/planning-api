a6e7398990dc863aa44ada9962ee946902e637d4
Kevin <kevin.manssat@hotmail.fr>
2020-06-30 18:49:26 +0200

    fix connexion

--- app/connexion.ts
+++ app/connexion.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export default new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@db:${process.env.DB_PORT}/ssdp`,
+  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   {
     sync: { force: true },
   },
