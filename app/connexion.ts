3bc3e6d6292117ae79bb0fd1dbf2a6f1777d3d35
ptp <paartheepan94@gmail.com>
2020-07-05 16:02:11 +0200

    chore: db name

--- app/connexion.ts
+++ app/connexion.ts
@@ -2,11 +2,12 @@ import { Sequelize } from 'sequelize';
 
 // pour avoir une base de données différente pour l'environement de test
 const dbName =
-  process.env.NODE_ENV === 'test' ? 'ssdp-test' : process.env.DB_NAME;
+  process.env.NODE_ENV === 'test' ? 'postgres' : process.env.DB_NAME;
 
 export default new Sequelize(
   `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${dbName}`,
   {
     sync: { force: true },
+    logging: false,
   },
 );
