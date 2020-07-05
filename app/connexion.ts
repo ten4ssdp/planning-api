85e3bf002c00472b0771dcc358449c676ba07bfe
ptp <paartheepan94@gmail.com>
2020-07-05 11:04:32 +0200

    chore: lint files

--- app/connexion.ts
+++ app/connexion.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from 'sequelize';
 
 // pour avoir une base de données différente pour l'environement de test
-const db_name =
+const dbName =
   process.env.NODE_ENV === 'test' ? 'ssdp-test' : process.env.DB_NAME;
 
 export default new Sequelize(
-  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${db_name}`,
+  `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${dbName}`,
   {
     sync: { force: true },
   },
